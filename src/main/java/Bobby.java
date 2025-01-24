967c46af066fa225df241c6918645512151deafa
CRL006 <chongruolin@gmail.com>
2025-01-24 11:10:34 +0800

    Bobby!

---
+++ src/main/java/Bobby.java
@@ -0,0 +1,8 @@
+public class Bobby {
+    public static void main(String[] args) {
+        String greeting = "Hello! I'm Bobby! \nWhat can I do for you? \n";
+        System.out.println(greeting);
+        String endchat = "Bye. Hope to see you again soon!";
+        System.out.println(endchat);
+    }
+}
