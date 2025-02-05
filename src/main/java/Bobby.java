111b6634a1160b35b9116bfd7b6e39433967ea1f
CRL006 <chongruolin@gmail.com>
2025-02-05 14:50:12 +0800

    level-2

--- src/main/java/Bobby.java
+++ src/main/java/Bobby.java
@@ -1,12 +1,25 @@
 import java.util.Scanner;  // Import the Scanner class
 public class Bobby {
     public static void main(String[] args) {
+        int tasksCounter = 0;
+        int markIndex;
         String greeting = "Hello! I'm Bobby! \nWhat can I do for you? \n";
         System.out.println(greeting);
+        String[] tasks = new String[100];
         Scanner scanner = new Scanner(System.in);  // Create a Scanner object
         String userInput = scanner.nextLine();
         while (!userInput.equals("bye")) {
-            System.out.println(userInput);
+            if (userInput.equals("list")) {
+                System.out.println("Here are the tasks in your list: ");
+                for (int i = 0; i < tasksCounter; i++) {
+                    System.out.print((i+1) + ". ");
+                    System.out.println(tasks[i]);
+                }
+            } else {
+                System.out.println("added: " + userInput);
+                tasks[tasksCounter] = userInput;
+                tasksCounter++;
+            }
             userInput = scanner.nextLine();
         }
         String endChat = "Bye. Hope to see you again soon!";
