8da630a7fd6af2373c11bab48d6d2d5a461154a2
CRL006 <chongruolin@gmail.com>
2025-02-05 12:01:58 +0800

    Level-1

--- src/main/java/Bobby.java
+++ src/main/java/Bobby.java
@@ -1,7 +1,15 @@
+import java.util.Scanner;  // Import the Scanner class
+
 public class Bobby {
     public static void main(String[] args) {
         String greeting = "Hello! I'm Bobby! \nWhat can I do for you? \n";
         System.out.println(greeting);
+        Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+        String userInput = myObj.nextLine();
+        while (!userInput.equals("bye")) {
+            System.out.println(userInput);
+            userInput = myObj.nextLine();
+        }
         String endChat = "Bye. Hope to see you again soon!";
         System.out.println(endChat);
     }
