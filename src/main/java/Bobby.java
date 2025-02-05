62569fb33ea0c0fd7146d9c595a4846fe506da91
CRL006 <chongruolin@gmail.com>
2025-02-05 11:45:50 +0800

    Revert "Bobby!"
    This reverts commit 967c46af066fa225df241c6918645512151deafa.

--- src/main/java/Bobby.java
+++
@@ -1,8 +0,0 @@
-public class Bobby {
-    public static void main(String[] args) {
-        String greeting = "Hello! I'm Bobby! \nWhat can I do for you? \n";
-        System.out.println(greeting);
-        String endchat = "Bye. Hope to see you again soon!";
-        System.out.println(endchat);
-    }
-}
