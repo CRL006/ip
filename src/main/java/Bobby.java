c1f20d47cd794232a472d20697c4da9ffc7c4b40
CRL006 <chongruolin@gmail.com>
2025-02-06 16:04:24 +0800

    level-4

--- src/main/java/Bobby.java
+++ src/main/java/Bobby.java
@@ -3,40 +3,83 @@ public class Bobby {
     public static void main(String[] args) {
         int tasksCounter = 0;
         int markIndex;
+        int stringLength;
         String greeting = "Hello! I'm Bobby! \nWhat can I do for you? \n";
         System.out.println(greeting);
         String[] tasks = new String[100];
         Boolean[] isDone = new Boolean[100];
+        int[] taskType = new int[100];
         Scanner scanner = new Scanner(System.in);  // Create a Scanner object
         String userInput = scanner.nextLine();
         while (!userInput.equals("bye")) {
             if (userInput.equals("list")) {
                 System.out.println("Here are the tasks in your list: ");
                 for (int i = 0; i < tasksCounter; i++) {
-                    System.out.print((i+1) + ". ");
+                    System.out.print((i + 1) + ". ");
+                    if (taskType[i] == 0) {
+                        System.out.print("[T]");
+                    } else if (taskType[i] == 1){
+                        System.out.print("[D]");
+                    } else {
+                        System.out.print("[E]");
+                    }
                     if (isDone[i]) {
                         System.out.print("[X] ");
-                    }
-                    else {
+                    } else {
                         System.out.print("[ ] ");
                     }
                     System.out.println(tasks[i]);
                 }
+            } else if (userInput.contains("todo")){
+                stringLength = userInput.length();
+                System.out.println("Got it. I've added this task: \n  [T][ ] " + userInput.substring(5, stringLength) + "\nNow you have " + (tasksCounter + 1) + " tasks in the list.");
+                tasks[tasksCounter] = userInput.substring(5, stringLength);
+                isDone[tasksCounter] = false;
+                taskType[tasksCounter] = 0;
+                tasksCounter++;
+            } else if (userInput.contains("deadline")){
+                stringLength = userInput.length();
+                int indexOfSlash = userInput.indexOf("/");
+                tasks[tasksCounter] = userInput.substring(9, indexOfSlash-1) + " (by: " + userInput.substring(indexOfSlash+4, stringLength) + ")";
+                isDone[tasksCounter] = false;
+                taskType[tasksCounter] = 1;
+                System.out.println("Got it. I've added this task: \n  [D][ ] " + tasks[tasksCounter] + "\nNow you have " + (tasksCounter + 1) + " tasks in the list.");
+                tasksCounter++;
+            } else if (userInput.contains("event")){
+                stringLength = userInput.length();
+                int indexOfFirstSlash = userInput.indexOf("/");
+                int indexOfLastSlash = userInput.lastIndexOf("/");
+                tasks[tasksCounter] = userInput.substring(6, indexOfFirstSlash-1) + " (from: " + userInput.substring(indexOfFirstSlash+6, indexOfLastSlash-1) + " to: " + userInput.substring(indexOfLastSlash + 4, stringLength) + ")";
+                System.out.println("Got it. I've added this task: \n  [E][ ] " + tasks[tasksCounter] + "\nNow you have " + (tasksCounter + 1) + " tasks in the list.");
+                isDone[tasksCounter] = false;
+                taskType[tasksCounter] = 2;
+                tasksCounter++;
             } else if (userInput.contains("unmark")) {
                 String[] splitMark = userInput.split(" ");
-                markIndex = Integer.parseInt(splitMark[1]);
-                isDone[markIndex-1] = false;
-                System.out.println("OK, I've marked this task as done: \n  [ ] " + tasks[markIndex - 1]);
+                markIndex = Integer.parseInt(splitMark[1]) - 1;
+                isDone[markIndex] = false;
+                System.out.println("OK, I've marked this task as not done yet:");
+                if (taskType[markIndex] == 0) {
+                    System.out.print("[T]");
+                } else if (taskType[markIndex] == 1){
+                    System.out.print("[D]");
+                } else {
+                    System.out.print("[E]");
+                }
+                System.out.println("[ ] " + tasks[markIndex]);
             } else if (userInput.contains("mark")) {
                 String[] splitMark = userInput.split(" ");
-                markIndex = Integer.parseInt(splitMark[1]);
-                isDone[markIndex-1] = true;
-                System.out.println("Nice! I've marked this task as done: \n  [X] " + tasks[markIndex -1]);
-            } else {
-                System.out.println("added: " + userInput);
-                tasks[tasksCounter] = userInput;
-                isDone[tasksCounter] = false;
-                tasksCounter++;
+                markIndex = Integer.parseInt(splitMark[1]) - 1;
+                isDone[markIndex] = true;
+                System.out.println("Nice! I've marked this task as done:");
+                if (taskType[markIndex] == 0) {
+                    System.out.print("[T]");
+                } else if (taskType[markIndex] == 1){
+                    System.out.print("[D]");
+                } else {
+                    System.out.print("[E]");
+                }
+                System.out.println("[X] " + tasks[markIndex]);
             }
             userInput = scanner.nextLine();
         }
